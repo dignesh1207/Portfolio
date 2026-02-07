927a0669a14ffd9e8c71b403295fc02794414e36
Dignesh Solanki <solank86@uwindsor.ca>
2026-02-06 21:58:48 -0500

    preset theme changed

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const themeToggle = document.getElementById('themeToggle');
 const html = document.documentElement;
 
 // Check for saved theme preference or default to light mode
-const currentTheme = localStorage.getItem('theme') || 'light';
+const currentTheme = localStorage.getItem('theme') || 'dark';
 html.setAttribute('data-theme', currentTheme);
 
 themeToggle.addEventListener('click', () => {
     const currentTheme = html.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     
     html.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
